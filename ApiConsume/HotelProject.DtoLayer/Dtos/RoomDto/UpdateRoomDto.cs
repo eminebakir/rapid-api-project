835ec04559c95bb27a6a5f7b3aa9afd497d93ad1
Emine <emine.bakir@authoritypartners.com>
2023-12-04 08:59:15 +0300

    Added Dto's

---
+++ ApiConsume/HotelProject.DtoLayer/Dtos/RoomDto/UpdateRoomDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.DtoLayer.Dtos.RoomDto
+{
+    public class UpdateRoomDto
+    {
+        public int RoomID { get; set; }
+        [Required(ErrorMessage = "Please add room number!")]
+        public string? RoomNumber { get; set; }
+        public string? RoomCoverImage { get; set; }
+        [Required(ErrorMessage = "Please add room price!")]
+        public int? Price { get; set; }
+        [Required(ErrorMessage = "Please add room title!")]
+        public string? Title { get; set; }
+        [Required(ErrorMessage = "Please add bed count of the room number!")]
+        public string? BedCount { get; set; }
+        [Required(ErrorMessage = "Please add bath count of the room number!")]
+        public string? BathCount { get; set; }
+        public string? Wifi { get; set; }
+        [Required(ErrorMessage = "Please add description!")]
+        public string? Description { get; set; }
+    }
+}
